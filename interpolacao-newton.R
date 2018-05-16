a86766cd05ba05872c057e97e4c18a30fc02d815
Luid Duarte <luidduarte@yahoo.com.br>
2018-05-16 16:17:15 -0300

    bug fix

--- interpolacao-newton.R
+++ interpolacao-newton.R
@@ -1,34 +1,31 @@
-#Interpolação de Newton
+##Interpolação de Newton
 
 interpolacao_newton <- function(X,Y){
-n <- length(X)
-i <- n
-j <- 1
-coeficientes <- c()
-
-while(i > 1){
-  coeficientes <-c(coeficientes, Y[1])
-  z <- c()
-  k <- 1
-  cat("Divisões:\n")
-  while(k < i){
-    cat(Y[1+k],"-",Y[k],"/",X[j+k],"-",X[k],"\n")
-    z <- c(z, ((Y[1+k]-Y[k])/(X[j+k]-X[k])))    
-    k <- k+1 
+  n <- length(X)
+  i <- n
+  j <- 1
+  coeficientes <- c()
+  
+  while(i >= 1){
+    coeficientes <-c(coeficientes, Y[1])
+    z <- c()
+    k <- 1
+    while(k < i){
+      z <- c(z, ((Y[1+k]-Y[k])/(X[j+k]-X[k])))
+      k <- k+1 
+    }
+    Y <- z
+    j <- j+1
+    i <- i-1
+    
   }
-  cat("resultados:\n", z, "\n\n")
-  Y <- z
-  j <- j+1
-  i <- i-1
-}
-cat("Coeficientes:", coeficientes)
-return(coeficientes)
+  return(coeficientes)
 }
 
 polinomio <- function(coeficientes, X, x){
   i <- 1
   resultado <- 0
-  while( i < length(X)){
+  while( i <= length(X)){
     j <- 1
     z <- 1
     while (j < i){
@@ -38,8 +35,10 @@ polinomio <- function(coeficientes, X, x){
     resultado <- resultado + coeficientes[i]*z
     i <- i+1
   }
-  cat("\n\nP(",x,") =",resultado)
   return(resultado)
   
 }
+
+
+
 
