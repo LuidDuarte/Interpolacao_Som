59a8fe1e8d46302512e59290c02a0886e808ffdc
unknown <luidduarte@yahoo.com.br>
2018-05-22 15:56:07 -0300

    agora o algoritmo funciona pra 21k de pontos, adicionado o campo de teste ao código .R também.

--- interpolacao-newton.R
+++ interpolacao-newton.R
@@ -6,8 +6,7 @@ interpolacao_newton <- function(X,Y){
   j <- 1
   coeficientes <- c()
   
-  while(i > 1){
-      cat(".")
+  while(i >= 1){
     coeficientes <-c(coeficientes, Y[1])
     z <- c()
     k <- 1
@@ -41,3 +40,32 @@ polinomio <- function(C, X, x){
   
   return(S[i-1])
 }
+
+
+
+matriz <-read.table(file="D:/3o\ Semestre/Calculo\ Numerico/Trabalho/notas\ piano/arquivos-texto/a1s.txt", sep="\t")
+X <- matriz$V1
+Y <- matriz$V2
+
+#A interpolação de newton com graus maiores chegará a resultados perto de infinito, e acabará nos levando a resultados incertos. 
+#A partir de testes, verifiquei que seria melhor dividir os vetores de 30 em 30, não gerando os erros de -Inf.
+n <- 1
+novo_Y_total <- c()
+while(n <= 20970){
+  m <- n+29
+  X <- matriz$V1[n:m]
+  Y <- matriz$V2[n:m]
+  
+  novo_Y <- c()
+  novo_X <- seq(0, X[length(X)], length.out = length(X))
+  coeficientes <- interpolacao_newton(X,Y)
+  a <- 1
+  while(a <= length(X)){
+    novo_Y <- c(novo_Y, polinomio(coeficientes, X, X[a]))
+    a<- a+1
+  }
+  novo_Y_total <- c(novo_Y_total, novo_Y)
+  n <- n+30
+}
+
+write(novo_Y_total, file="D:/3o\ Semestre/Calculo\ Numerico/Trabalho/notas\ piano/arquivos-texto/teste(2).txt", sep="\n")
