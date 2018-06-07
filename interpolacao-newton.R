225628d96d83a64ed924110ab9e05fc1ebf503ee
Luiz Guilherme Duarte <luidduarte@yahoo.com.br>
2018-06-07 03:18:33 -0300

    finalização de newton

--- interpolacao-newton.R
+++ interpolacao-newton.R
@@ -41,31 +41,66 @@ polinomio <- function(C, X, x){
   return(S[i-1])
 }
 
-
-
-matriz <-read.table(file="D:/3o\ Semestre/Calculo\ Numerico/Trabalho/notas\ piano/arquivos-texto/a1s.txt", sep="\t")
-X <- matriz$V1
-Y <- matriz$V2
-
-#A interpolação de newton com graus maiores chegará a resultados perto de infinito, e acabará nos levando a resultados incertos. 
-#A partir de testes, verifiquei que seria melhor dividir os vetores de 30 em 30, não gerando os erros de -Inf.
-n <- 1
-novo_Y_total <- c()
-while(n <= 20970){
-  m <- n+29
-  X <- matriz$V1[n:m]
-  Y <- matriz$V2[n:m]
+escreve <- function(nome_nota, grau, nome_resultado){
+  caminho <- "D:/3o\ Semestre/Calculo\ Numerico/Trabalho/notas\ piano/arquivos-texto/"
+  matriz <-read.table(file=paste(caminho,nome_nota, sep=""), sep="\t")
+  X <- matriz$V1
+  Y <- matriz$V2
   
-  novo_Y <- c()
-  novo_X <- seq(0, X[length(X)], length.out = length(X))
-  coeficientes <- interpolacao_newton(X,Y)
-  a <- 1
-  while(a <= length(X)){
-    novo_Y <- c(novo_Y, polinomio(coeficientes, X, X[a]))
-    a<- a+1
+  #A interpolação de newton com graus maiores chegará a resultados perto de infinito, e acabará nos levando a resultados incertos. 
+  #A partir de testes, verifiquei que seria melhor dividir os vetores de 30 em 30, não gerando os erros de -Inf.
+  n <- 1
+  i <- 1
+  frequencia <- 2.2675736961451E-5 #44100Hz
+  
+  inicio_vetor <- 0.00001
+  aux <- 1
+  
+  novo_X <- seq(0.00001, X[length(X)], 2.2675736961451E-5)
+  while(aux <= length(novo_X)){
+    novo_X[aux] <- round(novo_X[aux], digits=5)
+    aux <- aux+1
   }
-  novo_Y_total <- c(novo_Y_total, novo_Y)
-  n <- n+30
+  
+  teste <- 1
+  write (novo_X, file="D:/3o\ Semestre/Calculo\ Numerico/Trabalho/notas\ piano/arquivos-texto/novo_X.txt", sep="\n")
+  novo_Y_total <- c()
+  while(n <= 20970){
+    m <- n+grau
+    X <- matriz$V1[n:m]
+    Y <- matriz$V2[n:m]
+    
+    novo_Y <- c()
+    coeficientes <- interpolacao_newton(X,Y)
+    a <- 1
+    while(a <= length(X)){
+      if(novo_X[i] <= X[grau+1]){
+        novo_Y <- c(novo_Y, polinomio(coeficientes, X, novo_X[i]))
+        i <- i+1
+      }
+      else{
+        if(teste == 1){
+          cat("\nX[1] = ", X[1], "|novo_X = ", novo_X[i], " |X[30]=", X[4])
+        }
+        teste <- teste+1
+      }
+      a<- a+1
+      
+    }
+    novo_Y_total <- c(novo_Y_total, novo_Y)
+    n <- n+grau+1
+  }
+  caminhoResultado <- "D:/3o\ Semestre/Calculo\ Numerico/Trabalho/notas\ piano/arquivos-texto/resultados-newton/"
+  
+  write(novo_Y_total, file=paste(caminhoResultado,nome_resultado, sep=""), sep="\n")
+  
 }
 
-write(novo_Y_total, file="D:/3o\ Semestre/Calculo\ Numerico/Trabalho/notas\ piano/arquivos-texto/teste(2).txt", sep="\n")
+#main
+escreve("a1s.txt", 4,"a1s_grau_4.txt")
+
+
+
+
+
+
